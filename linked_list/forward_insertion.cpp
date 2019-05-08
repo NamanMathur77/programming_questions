ba310abb33f9953004d8463e701477823afec29d
NamanMathur77 <namanmathur619@gmail.com>
2019-05-08 22:12:07 +0530

    adding node from forward in linked list

---
+++ linked_list/forward_insertion.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+struct Node{
+int data;
+Node *next;
+};
+Node* head=NULL;
+void insert(int new_data){
+Node* new_node=new Node(); //struct Node* new_node=(struct Node*)malloc(sizeof(struct Node));
+new_node -> data=new_data; //new_node.data=new_data
+new_node -> next=head;     //new_node.next=head
+head=new_node;
+}
+void display(){
+Node* ptr;
+ptr=head;
+while(ptr!=NULL){
+cout<<ptr->data<<" ";
+ptr=ptr->next;
+}
+}
+int main(){
+insert(3);
+insert(2);
+insert(1);
+display();
+return 0;
+}
