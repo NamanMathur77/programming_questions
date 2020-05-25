ba4e39b76f5e294dfb5e2056f1d83cbc0c7e732d
NamanMathur77 <namanmathur619@gmail.com>
2020-05-25 23:31:24 +0530

    updating linked list in python

--- Object_Oriented_Programming/datastructures/linked_list_python.py
+++ Object_Oriented_Programming/datastructures/linked_list_python.py
@@ -8,6 +8,8 @@ class element:
 class LinkedList:
 	def __init__(self,head=None):
 		self.head = head
+
+# Adding the elements to the end on the linked list
 	def append(self, new_element):
 		current = self.head
 		if self.head:
@@ -16,6 +18,8 @@ class LinkedList:
 			current.next = new_element
 		else:
 			self.head = new_element
+
+# To delete the linked list elements
 	def delete(self, element):
 		current = self.head
 		while current.next:
@@ -24,6 +28,23 @@ class LinkedList:
 				current.next=next_element.next
 			current=current.next
 
+# adding element at a particular position in linked list
+	def add_element(self, value, position):
+		count=1
+		current=self.head
+		if position==1:
+			el=element(value)
+			el.next=current
+			self.head=el
+		else:
+			while current.next:
+				count=count+1
+				if count==position:
+					el=element(value)
+					el.next=current.next
+					current.next=el
+				current=current.next
+# To view the linked list elements
 	def view_linked_list(self):
 		current = self.head
 		while current.next:
@@ -37,6 +58,9 @@ for i in range(0, n):
 	num=int(input("Enter the element"))
 	el=element(num)
 	ll.append(el)
-num=int(input("Enter the element to delete"))
-ll.delete(num)
+#num=int(input("Enter the element to delete"))
+#ll.delete(num)
+num=int(input("Enter the number to add"))
+pos=int(input("Enter the position to add"))
+ll.add_element(num,pos)
 ll.view_linked_list()
